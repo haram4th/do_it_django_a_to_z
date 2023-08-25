bbfc073b110e6b914f40166b6d1bd603c09ff75b
haram4th <haram4th@gmail.com>
2023-08-25 16:48:51 +0900

    포스트 목록 페이지에 카테고리 기능 추가하기

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Category
 
 admin.site.register(Post)  # admin 페이지에 블로그 Post 작성이 가능해짐
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug' : ('name', )}
+
+admin.site.register(Category, CategoryAdmin)
