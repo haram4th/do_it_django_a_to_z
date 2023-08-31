f3358ec72eeab6f63c82ec672e27b7d3553e0dad
haram4th <haram4th@gmail.com>
2023-08-31 17:29:49 +0900

    docker-compose

--- do_it_django_prj/settings.py
+++ do_it_django_prj/settings.py
@@ -21,12 +21,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ')tfh(j7p9+muq3ll#-7g#r0m2af3u7#d_u$607gs35tk(bj6%j'
+SECRET_KEY = os.environ.get('SECRET_KEY', ')tfh(j7p9+muq3ll#-7g#r0m2af3u7#d_u$607gs35tk(bj6%j')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get('DEBUG', 1))
 
-ALLOWED_HOSTS = []
+if os.environ.get('DJANGO_ALLOWED_HOSTS'):
+    ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS').split(' ')
+else:    
+    ALLOWED_HOSTS = []
 
 
 # Application definition
